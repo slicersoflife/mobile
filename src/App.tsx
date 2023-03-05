74f87217fc506f828c1b7f886258c8818061d689
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2023-03-05 10:52:31 -0700

    feat: setup initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Login, Signup } from "@pages";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default registerRootComponent(App);
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export const Login = () => {
+  return (
+    <View>
+      <Text>Insert login page!</Text>
+    </View>
+  );
+};
